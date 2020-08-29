a2f6c7c6d404868534ee32a728de71b912072961
tsubasa namatame <nyshk97@gmail.com>
2020-08-29 16:34:33 +0900

    indexページのUIを修正

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,3 +1,15 @@
 class Draft < ApplicationRecord
   validates :content, length: { maximum: 140 }, presence: true
+
+  LIFE_SPAN_DAY = 3
+
+  def remaining_time
+    remaining_second = self.created_at.next_day(LIFE_SPAN_DAY) - Time.zone.now
+    remaining_hour = (remaining_second / 3600).floor
+    if remaining_hour.zero?
+      "#{(remaining_secound / 60)}分"
+    else
+      "#{remaining_hour}時間"
+    end
+  end
 end
